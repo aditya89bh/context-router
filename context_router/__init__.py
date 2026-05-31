55c255c94e41981e9d7c0b15fa343330487303fe
Aditya Bhatia <aditya89bh@gmail.com>
2026-06-01 02:18:14 +0530

    Add public package exports

--- context_router/__init__.py
+++ context_router/__init__.py
@@ -3,6 +3,7 @@
 from context_router.context.context_pack import ContextPack
 from context_router.context.context_types import ContextItem, ScoredContextItem
 from context_router.context.memory_store import MemoryStore
+from context_router.router.base import BaseRouter
 from context_router.router.hybrid_router import HybridRouter
 from context_router.router.recency_router import RecencyRouter
 from context_router.router.semantic_router import SemanticRouter
@@ -13,6 +14,7 @@ __all__ = [
     "ScoredContextItem",
     "ContextPack",
     "MemoryStore",
+    "BaseRouter",
     "RecencyRouter",
     "SemanticRouter",
     "TaskRouter",
